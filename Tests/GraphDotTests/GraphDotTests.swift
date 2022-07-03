f04eb6245f22fe539758da47ed604b023bf42cd8
Wolf McNally <wolf@wolfmcnally.com>
2022-07-03 14:48:16 -0700

    Cleanup.

--- Tests/GraphDotTests/GraphDotTests.swift
+++ Tests/GraphDotTests/GraphDotTests.swift
@@ -5,7 +5,7 @@ import GraphDot
 // Editor: http://magjac.com/graphviz-visual-editor/
 
 final class GraphDotTests: XCTestCase {
-    func testDot() throws {
+    func testDotGraph() throws {
         let graph = DotTestGraph.makeGraph()
         let dot = """
         digraph G {
@@ -42,7 +42,83 @@ final class GraphDotTests: XCTestCase {
         """
         XCTAssertEqual(graph.dotFormat, dot)
     }
-    
+
+    func testDotDAG() throws {
+        let graph = DotTestGraph.makeDAG()
+        let dot = """
+        digraph G {
+            A [label="A"]
+            B [label="B"]
+            C [label="C"]
+            D [label="D"]
+            E [label="E"]
+            F [label="F"]
+            G [label="G"]
+            H [label="H"]
+            I [label="I"]
+            J [label="J"]
+            K [label="K"]
+            A -> C [label="AC"]
+            A -> D [label="AD"]
+            A -> E [label="AE"]
+            B -> A [label="BA"]
+            B -> C [label="BC"]
+            B -> G [label="BG"]
+            C -> D [label="CD"]
+            E -> D [label="ED"]
+            F -> D [label="FD"]
+            F -> E [label="FE"]
+            I -> G [label="GI"]
+            H -> J [label="HJ"]
+            B -> I [label="IB"]
+            I -> C [label="IC"]
+            I -> K [label="IK"]
+            J -> A [label="JA"]
+            J -> E [label="JE"]
+            J -> F [label="JF"]
+        }
+        """
+        XCTAssertEqual(graph.dotFormat, dot)
+    }
+
+    func testDotTree() throws {
+        let graph = DotTestGraph.makeTree()
+        let dot = """
+        digraph G {
+            A [label="A"]
+            B [label="B"]
+            C [label="C"]
+            D [label="D"]
+            E [label="E"]
+            F [label="F"]
+            G [label="G"]
+            H [label="H"]
+            I [label="I"]
+            J [label="J"]
+            K [label="K"]
+            L [label="L"]
+            M [label="M"]
+            N [label="N"]
+            O [label="O"]
+            A -> B [label="AB"]
+            A -> C [label="AC"]
+            A -> D [label="AD"]
+            B -> I [label="BI"]
+            C -> H [label="CH"]
+            D -> E [label="DE"]
+            D -> F [label="DF"]
+            D -> G [label="DG"]
+            E -> M [label="EM"]
+            E -> N [label="EN"]
+            E -> O [label="EO"]
+            F -> L [label="FL"]
+            H -> J [label="HJ"]
+            H -> K [label="HK"]
+        }
+        """
+        XCTAssertEqual(graph.dotFormat, dot)
+    }
+
     func testDotAttributes() throws {
         var graph = DotTestGraph.makeDAG()
         try graph.newNode("Z")
