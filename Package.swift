dafe511afa3873e22c5cfd8478a9025d65480f99
Wolf McNally <wolf@wolfmcnally.com>
2023-01-07 15:12:58 -0800

    Removed platform requirements.

--- Package.swift
+++ Package.swift
@@ -4,15 +4,14 @@ import PackageDescription
 
 let package = Package(
     name: "GraphDot",
-    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         .library(
             name: "GraphDot",
             targets: ["GraphDot"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/WolfMcNally/WolfBase.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/WolfMcNally/Graph.git", branch: "master")
+        .package(url: "https://github.com/WolfMcNally/WolfBase.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/WolfMcNally/Graph.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         .target(
